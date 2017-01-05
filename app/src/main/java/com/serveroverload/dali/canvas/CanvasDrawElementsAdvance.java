2a0170991f96aecae902b050796c15afd08bad42
Hitesh Sahu <hiteshkumarsahu1990@gmail.com>
2017-01-05 11:41:08 +0530

    Fixed  issue with dependencies

--- app/src/main/java/com/serveroverload/dali/canvas/CanvasDrawElementsAdvance.java
+++ app/src/main/java/com/serveroverload/dali/canvas/CanvasDrawElementsAdvance.java
@@ -57,88 +57,39 @@ public class CanvasDrawElementsAdvance extends View {
         protected void onDraw(Canvas canvas) {
 
             canvas.save();
-            //X??? 10???
             canvas.translate(10, 0);
-            //????canvas ??? Notice ???translate? drawARGB????!!
             canvas.drawARGB(111, 156, 11, 44);
             // ????
             canvas.drawPoint(10, 10, mPointPaint);
 
-            //X??? 50???
             canvas.translate(50, 0);
-            //??? Notice ???translate(10,0) ?? translate(50,0) ????!
-            //Notice ?? ????????(0,0),????????????(60,0)!!
             canvas.drawLine(0, 0, 0, 300, mLinePaint);
             canvas.drawLine(0, 300, 300, 300, mLinePaint);
-            //??????
-//            canvas.drawRect(mArcRectF, mRectFPaint);
-//            canvas.drawRect(1, 2, 3, 4, mRectFPaint);
-
-//            canvas.translate(300, 0);
-//            canvas.drawRoundRect(mArcRectF,0,20,mRectFPaint);
-
-//            canvas.translate(300, 0);
-//            canvas.drawRoundRect(mArcRectF,50,70,mRectFPaint);
-
-            //????????????
-            //????????,??????(???)
-            /**
-             * Notice  ?? ?????:
-             *
-             *            270°(-90°)
-             *            |
-             *            |
-             *            |
-             * 180°       |
-             * ___________|____________ 0°
-             *           /|\a
-             *          /b| \
-             *         /  |  \
-             *        /   |   \
-             *       /    90°(-270°)
-             *
-             *
-             *  startAngle ?????????,?0 ??(a)
-             *  sweepAngle ????????(b)   Notice ??360°,??????.
-             *  ?? a ?90 b ?22 ??????? ??????? 90°?,?????112°?
-             *
-             *  useCenter true ??????
-             *
-             *  http://ww3.sinaimg.cn/large/98900c07gw1ewtt438wdej20e10c8aa7.jpg
-             */
-            // ???? ????
             canvas.drawArc(mArcRectF, 90, 22, true, mArcPaint);
             // ?????
             canvas.drawArc(mArcRectF, 122, 33, false, mArcPaint);
 
-//            canvas.drawArc(1,1,1,1,2,2,false,mArcPaint);
-
-
-//            //??? ??fill(??)
             mArcPaint.setStyle(Paint.Style.STROKE);
             canvas.drawArc(mArcRectF, 166, 44, true, mArcPaint);
-//
+
 
             canvas.restore();
             canvas.translate(0, 300);
-            //??360°,??????,??useCenter?false
             mArcPaint.setStrokeWidth(20);
-            mArcPaint.setStyle(Paint.Style.FILL_AND_STROKE);//???????????
+            mArcPaint.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawArc(mArcRectF, 22, 393, false, mArcPaint);
 
             canvas.translate(330, 100);
-            //?? ????,?? ??????
             mCirclePaint.setStyle(Paint.Style.FILL);
             canvas.drawCircle(50, 50, 50, mCirclePaint);
             mCirclePaint.setStyle(Paint.Style.STROKE);
             canvas.drawRect(0, 0, 100, 100, mCirclePaint);
 
             canvas.translate(400, 0);
-            mOvalRectF.set(0, 30, 267, 222);//??
+            mOvalRectF.set(0, 30, 267, 222);
             canvas.drawOval(mOvalRectF, mOvalPaint);
-//            canvas.drawOval(1, 1, 1, 1, mOvalPaint);
             canvas.translate(0, 300);
-            mOvalRectF.set(0, 0, 200, 200);//??????
+            mOvalRectF.set(0, 0, 200, 200);
             canvas.drawOval(mOvalRectF, mOvalPaint);
 
             canvas.translate(0, 300);
